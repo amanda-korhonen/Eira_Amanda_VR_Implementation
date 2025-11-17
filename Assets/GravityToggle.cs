3b1e94b53fd2157106ba323c6ed2553f35ff27e2
Amanda Korhonen <korhonen.amanda02@gmail.com>
2025-11-17 11:46:13 +0200

    Multiplayer + should fix gravity toggle

--- Assets/GravityToggle.cs
+++ Assets/GravityToggle.cs
@@ -1,9 +1,40 @@
 using UnityEngine;
+using Photon.Pun;
 
 public class GravityToggle : Interactive
 {
+    // Updated this to be able to see it in multiplayer on 17.11.2025
+    PhotonView pv;
+    private Rigidbody rb;
+    bool gravity = true;
+
+    void Start()
+    {
+        pv = GetComponent<PhotonView>();
+        rb = GetComponent<Rigidbody>();
+    }
+        
     public new void Interact()
     {
-        GetComponent<Rigidbody>().useGravity = true;
+        if (pv == null) return;
+        // When interacted, ask ALL players to kick the object
+        pv.RPC("SetGravity", RpcTarget.AllBuffered, gravity);
+    }
+
+    [PunRPC]
+
+    // The logic does not work should return the position
+    // of the cube and toggle off gravity
+    void SetGravity(bool gravity)
+    {
+        if(gravity == true)
+        {
+            rb.useGravity = true;
+        }else
+        {
+            rb.position = new Vector3(-1.27f, 1.8f, 1.81f);
+            rb.useGravity = false;
+            
+        }
     }
 }
